96060be93fbcd32b9965b768009805de5fb4db50
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-20 19:03:40 +0400

    Base application template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
-    h1 Hello Vue with Vuetify
+    v-app
+        v-navigation-drawer(app temporary)
+        v-app-bar(app dense dark color="primary")
+            v-app-bar-nav-icon
+            v-toolbar-title Ad application
+            v-spacer
+            v-btn(text) Один
+            v-btn(text) Два
+            v-btn(text) Три
+        v-content
+            |router-view
+        v-footer(app) &copy; 2020
 </template>
 
 <script>
