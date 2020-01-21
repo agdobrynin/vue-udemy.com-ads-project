355417869532505ecf3082ba72d0620e7ccc7af3
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-21 15:58:52 +0400

    Registration component stage 1

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,16 +1,60 @@
 <template lang="pug">
     v-container
-        v-layout(row)
-            v-flex()
-                h1 Registration
+        v-row(align="center" justify="center")
+            v-col(cols="12" sm="8" md="6")
+                v-card(class="elevation-12")
+                    v-toolbar(color="primary" dark flat)
+                        v-toolbar-title Регистрация
+                    v-spacer
+                    v-card-text
+                        v-form(v-model="valid" ref="form" validate)
+                            v-text-field(
+                                label="E-mail" type="email" name="email" v-model="email" prepend-icon="mdi-account"
+                                :rules="rulesEmail")
+                            v-text-field(
+                                id="password" label="Пароль" name="password" v-model="password" prepend-icon="mdi-lock" type="password"
+                                :counter="minPasswordLength"
+                                :rules="rulesPassword")
+                            v-text-field(
+                                id="password-confirm" label="Подтверждение пароля" name="password-confirm" v-model="passwordConfirm" prepend-icon="mdi-lock" type="password"
+                                :rules="rulesPasswordConfirm")
+                            v-card-actions
+                                v-spacer
+                                v-btn(color="primary" :disabled="!valid" @click="doRegistration") Войти
+
 </template>
 
 <script>
+    const MIN_PASSWORD_LENGTH = 6;
     export default {
-        name: "Registration"
+        name: "Login",
+        data () {
+            return {
+                minPasswordLength: MIN_PASSWORD_LENGTH,
+                valid: false,
+
+                email: "",
+                password: "",
+                passwordConfirm: "",
+                rulesEmail: [
+                    v => !!v || "E-mail обязательное поле",
+                    v => /.+@.+\.(\w{2,})/.test(v) || 'Значение не является email адресом',
+                ],
+                rulesPassword: [
+                    v => !!v || "Пароль обязательное поле",
+                    v => v.length >= this.minPasswordLength || `Пароля не менее ${this.minPasswordLength} символов`,
+                ],
+                rulesPasswordConfirm: [
+                    v => !!v || "Подтвердите пароль",
+                    v => v === this.password || `Пароли не совпадаю`,
+                ],
+            }
+        },
+        methods: {
+            doRegistration() {
+                // eslint-disable-next-line no-console
+                console.log(this.email, this.password);
+            }
+        },
     }
 </script>
-
-<style scoped>
-
-</style>
