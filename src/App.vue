ddd3eb5d1b8d22a6d0be08918df58486f709bc3f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-21 12:57:28 +0400

    Main template - navigation and menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
     v-app
-        v-navigation-drawer(app temporary)
+        v-navigation-drawer(app temporary v-model="navigationDrawer")
+            v-list(dense)
+                v-list-item(v-for="(item, index) in list" :key="index" :to="item.url")
+                    v-list-item-icon
+                        v-icon {{ item.icon }}
+                    v-list-item-content
+                        v-list-item-title {{ item.title }}
         v-app-bar(app dense dark color="primary")
-            v-app-bar-nav-icon
+            v-app-bar-nav-icon(@click="navigationDrawer = !navigationDrawer" class="hidden-sm-and-up")
             v-toolbar-title Ad application
             v-spacer
-            v-btn(text) Один
-                v-icon mdi-domain
-            v-btn(text) Два
-                v-icon mdi-message-text
-            v-btn(text) Три
-                v-icon mdi-arrow-up-bold-box-outline
+            v-btn(text v-for="(item, index) in list" :key="index" :to="item.url" class="hidden-sm-and-down")
+                v-icon(left) {{ item.icon }}
+                | {{ item.title }}
         v-content
             | router-view
         v-footer(app) &copy; 2020
@@ -20,8 +23,22 @@
 
     export default {
         name: "App",
-        data: () => ({
-            //
-        }),
+        data() {
+            return {
+                navigationDrawer: false,
+                list: [
+                    {
+                        icon: "mdi-lock",
+                        title: "Login",
+                        url: "/login",
+                    },
+                    {
+                        icon: "mdi-account-plus",
+                        title: "Register",
+                        url: "/register",
+                    }
+                ],
+            }
+        },
     };
 </script>
