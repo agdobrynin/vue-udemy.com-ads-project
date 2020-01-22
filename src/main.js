ccfe5cbc31b217bb50ff62c02d6a0ba97be7f5fc
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-22 22:55:02 +0400

    Firebase config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,18 @@ new Vue({
     vuetify,
     router,
     store,
-    render: h => h(App)
+    render: h => h(App),
+    created() {
+        firebase.initializeApp(
+            {
+                apiKey: process.env.FIREBASE_API_KEY,
+                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                databaseURL: process.env.FIREBASE_DATABASE_URL,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+                appId: process.env.FIREBASE_APP_ID,
+            }
+        );
+    },
 }).$mount("#app");
