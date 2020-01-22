10b2683b7bb3fae7f59d0d5931967a2b8ddbbe95
Alexey Dobrynin <ag.dobrynin@gmail.com>
2020-01-22 13:45:47 +0400

    Orders page fix template

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -4,17 +4,16 @@
             v-flex(sm6 offset-3)
                 v-list
                     v-toolbar-title.px-6 Заказы на покупку
-                    v-list-item-group(v-model='settings' multiple='')
+                    v-list-item-group(multiple)
                         v-list-item(v-for="(order, index) in orders" :key="index")
-                            template(v-slot:default='{ active, toggle }')
+                            template
                                 v-list-item-action
-                                    v-checkbox(v-model='active' color='primary' @click='toggle')
+                                    v-checkbox(:input-value="order.done" color="success" @click="doDone(order)")
                                 v-list-item-content
                                     v-list-item-title {{ order.name }}
                                     v-list-item-subtitle {{ order.phone }}
-                                v-list-item-action
-                                    v-btn(:to="{name: 'oneAdv', params:{id: order.id}}")
-                                        | Просмотр
+                            v-list-item-action
+                                v-btn(:to="{name: 'oneAdv', params:{id: order.adId}}") Просмотр
 </template>
 
 <script>
@@ -24,11 +23,25 @@
             orders: [
                 {
                     id: 123,
+                    adId: 321,
                     name: "Иван",
                     phone: "+7-927-236-56-89",
-                }
+                    done: false,
+                },
+                {
+                    id: 124,
+                    adId: 31982,
+                    name: "Пётр",
+                    phone: "+7-919-547-11-05",
+                    done: false,
+                },
             ],
         }),
+        methods: {
+            doDone: (order) => {
+                order.done = true;
+            },
+        },
     }
 </script>
 
